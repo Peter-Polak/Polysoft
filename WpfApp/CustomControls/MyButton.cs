66b8dd4903adf2333df6bee4fc939ac6ef1d4017
Peter Polák <peter.polak.mail@gmail.com>
2021-04-17 01:43:32 +0200

    Add custom control and style practice tests

---
+++ WpfApp/CustomControls/MyButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp.CustomControls
+{
+
+    public class MyButton : Control
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+                nameof(Text), typeof(string), typeof(MyButton),
+                new PropertyMetadata(default(string)));
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+        
+
+        public MyButton()
+        {
+            //DefaultStyleKeyProperty.OverrideMetadata(typeof(MyButton), new System.Windows.FrameworkPropertyMetadata(typeof(MyButton)));
+        }
+    }
+}
