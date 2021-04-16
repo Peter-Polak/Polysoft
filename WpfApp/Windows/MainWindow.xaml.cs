72a33c6a513bf65952c57ec56482bbbcceab15cf
Peter Polák <peter.polak.mail@gmail.com>
2021-04-16 20:21:10 +0200

    Refactor code and add documentation

--- WpfApp/Windows/MainWindow.xaml.cs
+++ WpfApp/Windows/MainWindow.xaml.cs
@@ -26,9 +26,19 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Keyboard simularator
+        /// </summary>
         private InputSimulator sim;
 
+        /// <summary>
+        /// Predefined states to choose from
+        /// </summary>
         public ObservableCollection<string> States { get; set; }
+
+        /// <summary>
+        /// Initialy chosen state
+        /// </summary>
         public string SelectedState { get; set; }
 
         public MainWindow()
@@ -53,27 +63,25 @@ namespace WpfApp
 
         private void fillFirstRowButton_Click(object sender, RoutedEventArgs e)
         {
-            fillFirsRow();
+            FillFirsRow();
         }
 
         private void insertNewCustomerButton_Click(object sender, RoutedEventArgs e)
         {
-            //button2.IsEnabled = false;
-            //this.Dispatcher.Invoke(() =>
-            //{
-            //    insertNewCustomer();
-            //});
+            InsertNewCustomer();
 
-            insertNewCustomer();
+            // Increment customer number
+            if (customerNumber.Text.Length > 0) customerNumber.textBox.Text = (Int32.Parse(customerNumber.textBox.Text) + 1).ToString();
         }
 
         private void fillFormButton_Click(object sender, RoutedEventArgs e)
         {
-            fillForm();
+            FillForm();
 
-            if (customerNumber.Text.Length > 0) customerNumber.textBox.Text = (Int32.Parse(customerNumber.textBox.Text) + 1).ToString();
+            // Increment invoice number
             if (invoiceNumber.Text.Length > 0) invoiceNumber.textBox.Text = (Int32.Parse(invoiceNumber.textBox.Text) + 1).ToString();
 
+            // Clear form
             customerName.textBox.Text = "";
 
             ico.textBox.Text = "";
@@ -82,37 +90,47 @@ namespace WpfApp
             amount.textBox.Text = "";
         }
 
-        private void fillFirsRow()
+        // Fill the first row of invoice (invoice ID), open customer database window and look up customer
+        private void FillFirsRow()
         {
             const int delay = 100;
 
-            IntPtr calcWindow = FindWindow(null, "Formulár knihy vyšlých faktúr");
-            SetForegroundWindow(calcWindow);
+            // Switch window to invoice form (window must be open)
+            IntPtr invoiceWindow = FindWindow(null, "Formulár knihy vyšlých faktúr");
+            SetForegroundWindow(invoiceWindow);
 
+            // Fill invoice indexes
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
             sim.Keyboard.Sleep(delay).TextEntry(year.Text.Substring(2, 2) + invoiceNumber.Text);
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
             sim.Keyboard.Sleep(delay).TextEntry(year.Text + invoiceNumber.Text); // Číslo faktúry
-
             sim.Keyboard.Sleep(delay + 100).KeyPress(VirtualKeyCode.RETURN);
+
+            // Open customer database window and search window
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
             sim.Keyboard.Sleep(delay + 1500).KeyPress(VirtualKeyCode.RIGHT); // Wait for window to open
             sim.Keyboard.Sleep(delay).ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.F7);
 
+            // Search for customer name in database of current customers
             sim.Keyboard.Sleep(delay).TextEntry(customerName.Text); // Názov odberateľa
-
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
         }
 
-        private void insertNewCustomer()
+        /// <summary>
+        /// Insert new customer to the database. Must have customer database window open and it must be the last window in focus.
+        /// </summary>
+        private void InsertNewCustomer()
         {
             const int delay = 100;
 
+            // Switch back to the customer database window
             sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LMENU, VirtualKeyCode.TAB);
 
-            sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.TAB);
+            // Open window for adding new customers to the database
+            sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.TAB); // Needed only if the customer database window was closed during addition of this invoice
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.INSERT);
 
+            // Format customer ID to have 4 digit (fill with zeros at the beggining until it is 4 digit long)
             if (customerNumber.Text.Length < 4)
             {
                 for (int i = 0; i < 4 - customerNumber.Text.Length; i++)
@@ -121,50 +139,63 @@ namespace WpfApp
                 }
             }
 
-            sim.Keyboard.Sleep(delay + 500);
-            //sim.Keyboard.Sleep(delay + 500).TextEntry(customerNumber.Text); // Číslo odberateľa // DOESN'T WORK
+            sim.Keyboard.Sleep(delay + 500); // Wait for window to open
+
+            // Fill customer ID
+            //sim.Keyboard.Sleep(delay + 500).TextEntry(customerNumber.Text); // Číslo odberateľa // DOESN'T WORK, have to use the loop to input the ID
             for (int i = 0; i < 4; i++)
             {
                 sim.Keyboard.Sleep(delay).TextEntry(customerNumber.Text[i]); // Číslo odberateľa
             }
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
 
+            // Fill customer name
             sim.Keyboard.Sleep(delay).TextEntry(customerName.Text); // Názov odberateľa
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
 
+            // Skip fields we don't need to fill
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
+
+            // Fill state
             sim.Keyboard.Sleep(delay).TextEntry(state.Text); // Štát
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
 
+            // Fill IČO if needed
             if (ico.Text.Length > 0) sim.Keyboard.Sleep(delay).TextEntry(ico.Text);
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN); // IČO
 
+            // Fill DIČ if needed
             if (ico.Text.Length > 0) sim.Keyboard.Sleep(delay).TextEntry(dic.Text);
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN); // DIČ
 
-            if(dic.Text.Length > 0)
+            // Fill IČ DPH if needed
+            if (dic.Text.Length > 0)
             {
                 sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
                 sim.Keyboard.Sleep(delay).TextEntry(icDph.Text);
             }
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN); // IČ DPH
 
+            // Fill account constant
             sim.Keyboard.Sleep(delay).TextEntry("3110001"); // ÚČet
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
 
+            // Fill type of document constant
             sim.Keyboard.Sleep(delay).TextEntry("VFA"); // Druh dokladu
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
 
+            // Fill group of customer constant
             sim.Keyboard.Sleep(delay).TextEntry("00"); // Skupina odberateľov
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
         }
 
-        private void fillForm()
+        // Fill the rest of the invoice. Must have customer database window open and it must be the last window in focus.
+        private void FillForm()
         {
             const int delay = 100;
 
@@ -199,24 +230,28 @@ namespace WpfApp
             sim.Keyboard.Sleep(delay).KeyPress(VirtualKeyCode.RETURN);
         }
 
+        // Event handler for closing application when Esc key is pressed.
         private void HandleEsc(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Escape)
                 Close();
         }
 
+        // Set date2 value to date1.
         private void dateCheckBox_Checked(object sender, RoutedEventArgs e)
         {
             date2Picker.SelectedDate = DateTime.Parse(date1Picker.Text);
             date2Picker.IsEnabled = false;
         }
 
+        // Set date2 value to date1 and offset it by 15 days.
         private void dateCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
             date2Picker.SelectedDate = DateTime.Parse(date1Picker.Text).AddDays(15);
             date2Picker.IsEnabled = true;
         }
 
+        // Set date2 value to date1 if check box is checked or set date2 value to date1 and offset it by 15 days.
         private void date1Picker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
             if(dateCheckBox.IsChecked ?? false)
@@ -229,6 +264,7 @@ namespace WpfApp
             }
         }
 
+        // Update IČ DPh field if the user is typing in DIČ field and format it (prepend SK at the start).
         private void dic_TextChanged(object sender, TextChangedEventArgs e)
         {
             string icDphEdited = "SK" + (sender as TextBox).Text;
