857eac2271678355f53788e7919e9507fb261c30
Peter Polák <peter.polak.mail@gmail.com>
2021-04-20 20:27:59 +0200

    Add models

---
+++ WpfApp/Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace WpfApp.Models
+{
+    public class Customer : ObservableObject
+    {
+        #region Private backing fields
+
+        private string _isDph;
+
+        #endregion
+
+        #region Properties
+
+        public string Name { get; set; }
+        public string Number { get; set; } = Properties.Settings.Default.CustomerNumber;
+        public string Ico { get; set; }
+        public string Dic { get; set; }
+        public string IcDph
+        {
+            get { return _isDph; }
+            set { _isDph = "SK" + value; }
+        }
+        public string Account { get; set; } = "3110001";
+        public string Type { get; set; } = "VFA";
+        public string Group { get; set; } = "00";
+
+        #endregion
+    }
+}
---
+++ WpfApp/Models/Invoice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Models
+{
+    public class Invoice : ObservableObject
+    {
+        #region Properties
+
+        public int Year { get; set; } = Int32.Parse(Properties.Settings.Default.Year);
+        public string Number { get; set; } = Properties.Settings.Default.InvoiceNumber;
+
+        public Customer Customer { get; set; }
+        public double Amount { get; set; }
+        public DateTime DateOfPayment { get; set; }
+        public DateTime DueDate { get; set; }
+
+        #endregion
+    }
+}
