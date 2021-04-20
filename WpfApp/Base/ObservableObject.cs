58e2f83e03ebb4c4c2d0b8a0a0959dd97c6c6158
Peter Polák <peter.polak.mail@gmail.com>
2021-04-20 23:37:09 +0200

    Move file to separate folder

---
+++ WpfApp/Base/ObservableObject.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace WpfApp.Base
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
